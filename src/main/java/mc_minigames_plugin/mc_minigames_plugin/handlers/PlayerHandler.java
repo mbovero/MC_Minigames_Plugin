96e1fa530408681c1fa5850e721209ee2a2f8cd2
mbovero <milesbovero@icloud.com>
2023-03-05 17:11:40 -0700

    Much improvement to player reset on join

--- src/main/java/mc_minigames_plugin/mc_minigames_plugin/handlers/PlayerHandler.java
+++ src/main/java/mc_minigames_plugin/mc_minigames_plugin/handlers/PlayerHandler.java
@@ -4,6 +4,7 @@ import mc_minigames_plugin.mc_minigames_plugin.MC_Minigames_Plugin;
 import mc_minigames_plugin.mc_minigames_plugin.util.DelayedTask;
 import mc_minigames_plugin.mc_minigames_plugin.util.Locations;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,14 +13,17 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.scoreboard.Team;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 import static mc_minigames_plugin.mc_minigames_plugin.util.Tools.createItem;
@@ -34,55 +38,84 @@ public class PlayerHandler implements Listener {
         Player player = event.getPlayer();
     }
 
+    /**
+     * Reset the player and their data when they join the server
+     */
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
+    public void resetOnPlayerJoin(PlayerJoinEvent event) {
         // Setup & retrieve data
         Player player = event.getPlayer();
         Inventory inv = player.getInventory();
 
-        // Tp player to hub
-        player.teleport(Locations.hub);
-        // Clear inventory
-        inv.clear();
-        // Clear potion effects
-        Collection<PotionEffect> effectsToClear = player.getActivePotionEffects();
-        for (PotionEffect pE : effectsToClear)
-            player.removePotionEffect(pE.getType());
-        // Clear player tags  -  change to handle actions based on current tags
-        Set<String> tagsToRemove = player.getScoreboardTags();
-        for (String tag : tagsToRemove)
-            if (!tag.equals("GameDev"))
-                player.removeScoreboardTag(tag);
+        // Reset player data on join -----------------------------------------------------------------------------------
+        // Unless troubleshooting...
+        if (!player.getScoreboardTags().contains("Testing")) {
+            // Tp player to hub
+            player.teleport(Locations.hub);
+            // Set to adventure mode
+            player.setGameMode(GameMode.ADVENTURE);
+            // Prevent/reset flying
+            player.setAllowFlight(false);
+            player.setFlying(false);
+            // Clear inventory
+            inv.clear();
+            // Clear potion effects
+            Collection<PotionEffect> effectsToClear = player.getActivePotionEffects();
+            for (PotionEffect pE : effectsToClear)
+                player.removePotionEffect(pE.getType());
+            // Reset health
+            player.resetMaxHealth();
+            player.setHealth(player.getMaxHealth());
+            // Reset hunger
+            player.setFoodLevel(20);
+            // Reset player scores
+            Bukkit.getScoreboardManager().getMainScoreboard().resetScores(player);      // Resets all objective scores for player - get individual objectives and player score object to change individual scores
+            // Remove player from team
+            Team team = Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(player);
+            if (team != null)
+                Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(player).removePlayer(player);
+            // Clear player tags  -  change to handle actions based on current tags
+            Set<String> tagsToRemove = new HashSet<>(player.getScoreboardTags());       // Get copy of player's current tags
+            for (String tag : tagsToRemove)
+                if (!tag.equals("GameDev")) {      // TEMP... only used for data pack capabilities
+                    player.removeScoreboardTag(tag);
+                    Bukkit.getLogger().info("removed " + tag);
+                }
+            // Give mainHub tag
+            player.addScoreboardTag("mainHub");
 
 
-        // Remove from teams
-
-        // Reset display name
+            // Reset display name
 
 
-
-        // Main hub menu selection
-        ItemStack item = createItem(new ItemStack(Material.COMPASS), "&aLobby Selector", "&fExplore our selection of games!");
-        inv.setItem(4, item);
-
-
+            // Lobby selection tool ----------------------------------------------------------------------------------------
+            ItemStack item = createItem(new ItemStack(Material.COMPASS), "&aLobby Selector", "&fExplore our selection of games!");
+            inv.setItem(4, item);
+        }
     }
 
     /**
-     * Opens/formats the player's Game Selector menu
+     * Opens/formats the player's Lobby Selector menu when the item is right-clicked
      */
     @EventHandler
     public void onPlayerUseMenu(PlayerInteractEvent event) {
         Player player = event.getPlayer();
+        // Detect when player right clicks
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)
+            // Detect right click with Lobby Selector compass
             if (player.getItemInHand().getItemMeta() != null && player.getItemInHand().getItemMeta().getDisplayName().equals("§aLobby Selector")) {
 
+                // Create "UI"
                 Inventory inv = Bukkit.createInventory(player, 9 * 3, "Lobby Selector");
-
+                // UI options:
+                // Main Hub
                 inv.setItem(11, createItem(new ItemStack(Material.RED_BED), "&2Main Hub","&7Home sweet home"));
+                // KOTH lobby
                 inv.setItem(13, createItem(new ItemStack(Material.GRASS_BLOCK), "&aKOTH","&7Conquer the Hill"));
+                // MM lobby
                 inv.setItem(15, createItem(new ItemStack(Material.DIAMOND_SWORD), "&cMurder Mystery","&7Stab your friends! :D"));
 
+                // Open the created "UI"
                 player.openInventory(inv);
             }
     }
@@ -92,20 +125,23 @@ public class PlayerHandler implements Listener {
      */
     @EventHandler
     public void onMenuClick(InventoryClickEvent event) {
-        // Only handle inv clicks if player is in /menu inventory
+        // Only handle inv clicks if player is in Lobby Selector inventory
         if (event.getView().getTitle().equals("Lobby Selector")) {
 
             // Retrieve the slot number that the player clicked on
             Player player = (Player) event.getWhoClicked();
             int slot = event.getSlot();
 
-            // Make sure an item was clicked
+            // Make sure an inventory item was clicked
             if (event.getCurrentItem() != null) {
-                // Execute correlating operation
+                // Send player to...
+                // Main Hub
                 if (slot == 11 && event.getCurrentItem().getItemMeta().getDisplayName().equals("§2Main Hub"))
                     player.teleport(Locations.hub);
+                // KOTH lobby
                 else if (slot == 13 && event.getCurrentItem().getItemMeta().getDisplayName().equals("§aKOTH"))
                     player.teleport(Locations.KOTHhub);
+                // MM lobby
                 else if (slot == 15 && event.getCurrentItem().getItemMeta().getDisplayName().equals("§cMurder Mystery"))
                     player.teleport(Locations.MMhub);
 
@@ -116,33 +152,54 @@ public class PlayerHandler implements Listener {
         }
     }
 
-
-
-
-
-
+    /**
+     * Prevents players from dropping items in lobbies
+     */
     @EventHandler
-    public void preventHunger(FoodLevelChangeEvent event) {
-        event.setCancelled(true);
+    public void preventItemDrop(PlayerDropItemEvent event) {
+        Player player = event.getPlayer();
+        Set<String> tags = player.getScoreboardTags();
+        if (tags.contains("mainHub"))
+            event.setCancelled(true);
     }
 
+    /**
+     * Prevents hunger in lobbies
+     */
     @EventHandler
-    public void onEntityDamage(EntityDamageEvent event) {
-        if (event.getCause() == EntityDamageEvent.DamageCause.FALL)
-            event.setCancelled(true);
+    public void preventHunger(FoodLevelChangeEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            Set<String> tags = player.getScoreboardTags();
+            if (tags.contains("mainHub"))
+                event.setCancelled(true);
+        }
+    }
 
-
-
-
-        // Ensure that a player was hurt by fall damage
-        if (!(event.getEntity() instanceof Player && event.getCause() == EntityDamageEvent.DamageCause.FALL)) {
-            return;
+    /**
+     * Prevents player damage in lobbies
+     */
+    @EventHandler
+    public void preventDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            Set<String> tags = player.getScoreboardTags();
+            if (tags.contains("mainHub"))
+                event.setCancelled(true);
         }
 
-        // Give a player a diamond after five seconds
-        Player player = (Player) event.getEntity();
-        DelayedTask task = new DelayedTask(() -> {player.getInventory().addItem(new ItemStack(Material.DIAMOND));}, 20 * 5);
-        // Cancel the task
-        Bukkit.getScheduler().cancelTask(task.getId());
+
+
+
+        // Tutorial stuff
+//        // Ensure that a player was hurt by fall damage
+//        if (!(event.getEntity() instanceof Player && event.getCause() == EntityDamageEvent.DamageCause.FALL)) {
+//            return;
+//        }
+//
+//        // Give a player a diamond after five seconds
+//        DelayedTask task = new DelayedTask(() -> {player.getInventory().addItem(new ItemStack(Material.DIAMOND));}, 20 * 5);
+//        // Cancel the task
+//        Bukkit.getScheduler().cancelTask(task.getId());
     }
 }
