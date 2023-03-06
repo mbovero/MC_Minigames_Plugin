3a9800970a56dabd8db21b030d4dd3e04ac7a35f
mbovero <milesbovero@icloud.com>
2023-03-06 16:42:20 -0700

    Added queue/dequeue item to MM lobby GameLobbyHandler now initializes red, blue, green, and yellow team for a KOTH game Players are now removed from all teams when they switch lobbies Improved troubleshooting abilities with "testing" tag Added team selector to MM and KOTH  - probably needs some more thorough testing   - we should mess with delays in hot bar menu usage cause its kinda quick rn, but that would require delays when teleporting and stuff too

--- src/main/java/mc_minigames_plugin/mc_minigames_plugin/handlers/GameLobbyHandler.java
+++ src/main/java/mc_minigames_plugin/mc_minigames_plugin/handlers/GameLobbyHandler.java
@@ -1,11 +1,10 @@
 package mc_minigames_plugin.mc_minigames_plugin.handlers;
 
 import mc_minigames_plugin.mc_minigames_plugin.MC_Minigames_Plugin;
-import mc_minigames_plugin.mc_minigames_plugin.minigames.Game;
-import mc_minigames_plugin.mc_minigames_plugin.minigames.PlayerArea;
 import mc_minigames_plugin.mc_minigames_plugin.util.Locations;
 import mc_minigames_plugin.mc_minigames_plugin.util.Tools;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -13,12 +12,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.world.WorldEvent;
 import org.bukkit.event.world.WorldInitEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scoreboard.NameTagVisibility;
 
-import java.util.ArrayList;
 import java.util.Set;
 
 import static mc_minigames_plugin.mc_minigames_plugin.util.Tools.createItem;
@@ -32,12 +30,41 @@ import static mc_minigames_plugin.mc_minigames_plugin.util.Tools.createItem;
  */
 public class GameLobbyHandler implements Listener {
 
-    ArrayList<PlayerArea> gameList;
+    // KOTH lobby hot bar menu items
     static ItemStack KOTHQueue = createItem(new ItemStack(Material.GRAY_DYE), "&7Unready", "&fClick with this item to enter the KOTH queue!");
     static ItemStack KOTHDequeue = createItem(new ItemStack(Material.LIME_DYE), "&aReady", "&fClick with this item to leave the KOTH queue");
 
+    static ItemStack KOTHTeamNone = createItem(new ItemStack(Material.LIGHT_GRAY_WOOL), "&7No Team", "&fClick with this item to change KOTH teams!");
+    static ItemStack KOTHTeamRed = createItem(new ItemStack(Material.RED_WOOL), "&4Red Team", "&fClick with this item to change KOTH teams!");
+    static ItemStack KOTHTeamBlue = createItem(new ItemStack(Material.BLUE_WOOL), "&1Blue Team", "&fClick with this item to change KOTH teams!");
+    static ItemStack KOTHTeamGreen = createItem(new ItemStack(Material.LIME_WOOL), "&2Green Team", "&fClick with this item to change KOTH teams!");
+    static ItemStack KOTHTeamYellow = createItem(new ItemStack(Material.YELLOW_WOOL), "&eYellow Team", "&fClick with this item to change KOTH teams!");
+
+
+
+    // MM lobby hot bar menu items
+    static ItemStack MMQueue = createItem(new ItemStack(Material.GRAY_DYE), "&7Unready", "&fClick with this item to enter the MM queue!");
+    static ItemStack MMDequeue = createItem(new ItemStack(Material.LIME_DYE), "&aReady", "&fClick with this item to leave the MM queue");
+
+    static ItemStack MMTeamNone = createItem(new ItemStack(Material.LIGHT_GRAY_WOOL), "&7No Team", "&fClick with this item to change MM teams!");
+    static ItemStack MMTeamRed = createItem(new ItemStack(Material.RED_WOOL), "&4Red Team", "&fClick with this item to change MM teams!");
+    static ItemStack MMTeamBlue = createItem(new ItemStack(Material.BLUE_WOOL), "&1Blue Team", "&fClick with this item to change MM teams!");
+    static ItemStack MMTeamGreen = createItem(new ItemStack(Material.LIME_WOOL), "&2Green Team", "&fClick with this item to change MM teams!");
+    static ItemStack MMTeamYellow = createItem(new ItemStack(Material.YELLOW_WOOL), "&eYellow Team", "&fClick with this item to change MM teams!");
+
     public GameLobbyHandler(MC_Minigames_Plugin plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
+        // Create KOTH teams
+        Tools.newTeam(Bukkit.getScoreboardManager().getMainScoreboard(), "KOTHRed", " ⧫ ", "Red", null, ChatColor.RED,false, true, NameTagVisibility.ALWAYS);
+        Tools.newTeam(Bukkit.getScoreboardManager().getMainScoreboard(), "KOTHBlue", " ⧫ ", "Blue", null, ChatColor.BLUE,false, true, NameTagVisibility.ALWAYS);
+        Tools.newTeam(Bukkit.getScoreboardManager().getMainScoreboard(), "KOTHGreen", " ⧫ ", "Green", null, ChatColor.GREEN,false, true, NameTagVisibility.ALWAYS);
+        Tools.newTeam(Bukkit.getScoreboardManager().getMainScoreboard(), "KOTHYellow", " ⧫ ", "Yellow", null, ChatColor.YELLOW,false, true, NameTagVisibility.ALWAYS);
+        // Create MM teams
+        Tools.newTeam(Bukkit.getScoreboardManager().getMainScoreboard(), "MMRed", " ⧫ ", "Red", null, ChatColor.RED,false, true, NameTagVisibility.ALWAYS);
+        Tools.newTeam(Bukkit.getScoreboardManager().getMainScoreboard(), "MMBlue", " ⧫ ", "Blue", null, ChatColor.BLUE,false, true, NameTagVisibility.ALWAYS);
+        Tools.newTeam(Bukkit.getScoreboardManager().getMainScoreboard(), "MMGreen", " ⧫ ", "Green", null, ChatColor.GREEN,false, true, NameTagVisibility.ALWAYS);
+        Tools.newTeam(Bukkit.getScoreboardManager().getMainScoreboard(), "MMYellow", " ⧫ ", "Yellow", null, ChatColor.YELLOW,false, true, NameTagVisibility.ALWAYS);
+
     }
 
     @EventHandler
@@ -47,12 +74,14 @@ public class GameLobbyHandler implements Listener {
 
 
     /*
-     * Lobby/hub tags for keeping track of player location:
+     * Lobby/hub tags for keeping track of player location/status:
      *
      *  - "testing"  - does not perform usual resets to player
      *  - "mainHub"
      *  - "KOTHLobby"
+     *  - "KOTHQueued"
      *  - "MMLobby"
+     *  - "MMQueued"
      */
 
     /**
@@ -63,10 +92,15 @@ public class GameLobbyHandler implements Listener {
     public static void sendMainHub(Player player) {
         // Tp player
         player.teleport(Locations.mainHub);
-        // Reset/set tags
+        // Reset tags
+        Set<String> tags = player.getScoreboardTags();
         Tools.resetTags(player);
-        player.addScoreboardTag("mainHub");
-        player.addScoreboardTag("notInGame");
+        // Set tags
+        player.addScoreboardTag("mainHub");         // Player is now in main hub
+        if (!tags.contains("testing"))              // Only apply tag if not troubleshooting
+            player.addScoreboardTag("notInGame");
+        // Reset team
+        Tools.resetTeam(player);
         // Reset/set inventory
         Inventory inv = player.getInventory();
         if (!player.getScoreboardTags().contains("testing"))    // Only clear inventory if not troubleshooting
@@ -82,15 +116,21 @@ public class GameLobbyHandler implements Listener {
     public static void sendKOTHLobby(Player player) {
         // Tp player
         player.teleport(Locations.KOTHLobby);
-        // Reset/set tags
+        // Reset tags
+        Set<String> tags = player.getScoreboardTags();
         Tools.resetTags(player);
-        player.addScoreboardTag("KOTHLobby");
-        player.addScoreboardTag("notInGame");
+        // Set tags
+        player.addScoreboardTag("KOTHLobby");         // Player is now in KOTH lobby
+        if (!tags.contains("testing"))                // Only apply tag if not troubleshooting
+            player.addScoreboardTag("notInGame");
+        // Reset team
+        Tools.resetTeam(player);
         // Reset/set inventory
         Inventory inv = player.getInventory();
         if (!player.getScoreboardTags().contains("testing"))    // Only clear inventory if not troubleshooting
             inv.clear();
         inv.setItem(0, KOTHQueue);
+        inv.setItem(2, KOTHTeamNone);
         inv.setItem(4, createItem(new ItemStack(Material.COMPASS), "&aLobby Selector", "&fExplore our selection of games!"));
     }
 
@@ -102,14 +142,20 @@ public class GameLobbyHandler implements Listener {
     public static void sendMMLobby(Player player) {
         // Tp player
         player.teleport(Locations.MMLobby);
-        // Reset/set tags
+        // Reset tags
+        Set<String> tags = player.getScoreboardTags();
         Tools.resetTags(player);
-        player.addScoreboardTag("MMLobby");
-        player.addScoreboardTag("notInGame");
+        // Set tags
+        player.addScoreboardTag("MMLobby");         // Player is now in MM lobby
+        if (!tags.contains("testing"))              // Only apply tag if not troubleshooting
+            player.addScoreboardTag("notInGame");
+        // Reset team
+        Tools.resetTeam(player);
         // Reset/set inventory
         Inventory inv = player.getInventory();
         if (!player.getScoreboardTags().contains("testing"))    // Only clear inventory if not troubleshooting
             inv.clear();
+        inv.setItem(0, MMQueue);
         inv.setItem(4, createItem(new ItemStack(Material.COMPASS), "&aLobby Selector", "&fExplore our selection of games!"));
     }
 
@@ -118,15 +164,19 @@ public class GameLobbyHandler implements Listener {
      */
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
+        // Setup
         Player player = event.getPlayer();
         Inventory inv = player.getInventory();
         Set<String> tags = player.getScoreboardTags();
+
+        // For all players in the KOTH Lobby...
         if (tags.contains("KOTHLobby")) {
             // Detect when player clicks
             if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK)
-                // Detect right click with an item
+                // Detect click with an item
                 if (player.getItemInHand().getItemMeta() != null) {
-                    // Detect right click with KOTH queue item
+                // QUEUE ITEM
+                    // Detect click with KOTH queue item
                     if (player.getItemInHand().getItemMeta().getDisplayName().equals(KOTHQueue.getItemMeta().getDisplayName())) {
                         // Queue player
                         player.addScoreboardTag("KOTHQueued");
@@ -135,7 +185,7 @@ public class GameLobbyHandler implements Listener {
                         // Play sound
                         player.playSound(player, Sound.BLOCK_NOTE_BLOCK_CHIME, 5, 1.5f);
                     }
-                    // Detect right click with KOTH dequeue item
+                    // Detect click with KOTH dequeue item
                     else if (player.getItemInHand().getItemMeta().getDisplayName().equals(KOTHDequeue.getItemMeta().getDisplayName())) {
                         // Dequeue player
                         player.removeScoreboardTag("KOTHQueued");
@@ -144,8 +194,134 @@ public class GameLobbyHandler implements Listener {
                         // Play sound
                         player.playSound(player, Sound.BLOCK_NOTE_BLOCK_COW_BELL, 5, .8f);
                     }
+
+                // TEAM SELECTOR
+                    // Detect click with KOTH team selector (none)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(KOTHTeamNone.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Bukkit.getScoreboardManager().getMainScoreboard().getTeam("KOTHRed").addPlayer(player);
+                        // Switch to next item
+                        inv.setItem(2, KOTHTeamRed);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+                    // Detect click with KOTH team selector (red)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(KOTHTeamRed.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Bukkit.getScoreboardManager().getMainScoreboard().getTeam("KOTHBlue").addPlayer(player);
+                        // Switch to next item
+                        inv.setItem(2, KOTHTeamBlue);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+                    // Detect click with KOTH team selector (blue)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(KOTHTeamBlue.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Bukkit.getScoreboardManager().getMainScoreboard().getTeam("KOTHGreen").addPlayer(player);
+                        // Switch to next item
+                        inv.setItem(2, KOTHTeamGreen);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+                    // Detect click with KOTH team selector (green)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(KOTHTeamGreen.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Bukkit.getScoreboardManager().getMainScoreboard().getTeam("KOTHYellow").addPlayer(player);
+                        // Switch to next item
+                        inv.setItem(2, KOTHTeamYellow);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+                    // Detect click with KOTH team selector (yellow)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(KOTHTeamYellow.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Tools.resetTeam(player);
+                        // Switch to next item
+                        inv.setItem(2, KOTHTeamNone);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+
+
                 }
         }
+
+        // For all players in the MM Lobby...
+        if (tags.contains("MMLobby")) {
+            // Detect when player clicks
+            if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK)
+                // Detect click with an item
+                if (player.getItemInHand().getItemMeta() != null) {
+                // QUEUE ITEM
+                    // Detect click with KOTH queue item
+                    if (player.getItemInHand().getItemMeta().getDisplayName().equals(MMQueue.getItemMeta().getDisplayName())) {
+                        // Queue player
+                        player.addScoreboardTag("MMQueued");
+                        // Switch to dequeue item
+                        inv.setItem(0, MMDequeue);
+                        // Play sound
+                        player.playSound(player, Sound.BLOCK_NOTE_BLOCK_CHIME, 5, 1.5f);
+                    }
+                    // Detect click with KOTH dequeue item
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(MMDequeue.getItemMeta().getDisplayName())) {
+                        // Dequeue player
+                        player.removeScoreboardTag("MMQueued");
+                        // Switch to queue item
+                        inv.setItem(0, MMQueue);
+                        // Play sound
+                        player.playSound(player, Sound.BLOCK_NOTE_BLOCK_COW_BELL, 5, .8f);
+                    }
+
+                    // TEAM SELECTOR
+                    // Detect click with MM team selector (none)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(MMTeamNone.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Bukkit.getScoreboardManager().getMainScoreboard().getTeam("MMRed").addPlayer(player);
+                        // Switch to next item
+                        inv.setItem(2, MMTeamRed);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+                    // Detect click with MM team selector (red)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(MMTeamRed.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Bukkit.getScoreboardManager().getMainScoreboard().getTeam("MMBlue").addPlayer(player);
+                        // Switch to next item
+                        inv.setItem(2, MMTeamBlue);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+                    // Detect click with MM team selector (blue)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(MMTeamBlue.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Bukkit.getScoreboardManager().getMainScoreboard().getTeam("MMGreen").addPlayer(player);
+                        // Switch to next item
+                        inv.setItem(2, MMTeamGreen);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+                    // Detect click with MM team selector (green)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(MMTeamGreen.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Bukkit.getScoreboardManager().getMainScoreboard().getTeam("MMYellow").addPlayer(player);
+                        // Switch to next item
+                        inv.setItem(2, MMTeamYellow);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+                    // Detect click with MM team selector (yellow)
+                    else if (player.getItemInHand().getItemMeta().getDisplayName().equals(MMTeamYellow.getItemMeta().getDisplayName())) {
+                        // Put player on red team
+                        Tools.resetTeam(player);
+                        // Switch to next item
+                        inv.setItem(2, MMTeamNone);
+                        // Play sound
+                        player.playSound(player, Sound.ITEM_ARMOR_EQUIP_LEATHER, 5, 1);
+                    }
+
+                }
+        }
+
     }
 
 
--- src/main/java/mc_minigames_plugin/mc_minigames_plugin/util/Tools.java
+++ src/main/java/mc_minigames_plugin/mc_minigames_plugin/util/Tools.java
@@ -5,6 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scoreboard.NameTagVisibility;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -40,12 +43,62 @@ public class Tools {
         return item;
     }
 
+    /**
+     * Method to facilitate the creation of new teams.
+     * Inputting "null" for certain parameters will result in default settings.
+     *
+     * @param scoreboard the scoreboard to add the new team to
+     * @param teamName the team name used by java (don't use color codes)
+     * @param prefix the text to be displayed before team members' names (color code capability)
+     * @param displayName the name of the team to be displayed in game (color code capability)
+     * @param suffix the text ot be displayed after team members' names (color code capability)
+     * @param teamColor the color of the team
+     * @param friendlyFire whether players on this team can attack each other or not
+     * @param seeInvisibleFriends whether players on this team can see invisible teammates or not
+     * @param nameTagVisibility the visibility of players' name tags (ALWAYS, HIDE FOR OTHER TEAMS, HIDE FOR OWN TEAM, NEVER)
+     */
+    public static void newTeam(Scoreboard scoreboard, String teamName, String prefix, String displayName, String suffix, ChatColor teamColor, boolean friendlyFire, boolean seeInvisibleFriends, NameTagVisibility nameTagVisibility) {
+        if (scoreboard.getTeam(teamName) != null) {
+            Bukkit.getLogger().info(teamName + " was not created because it already exists.");
+            return;
+        }
+        scoreboard.registerNewTeam(teamName);
+        Team team = scoreboard.getTeam(teamName);
+        team.setAllowFriendlyFire(friendlyFire);
+        team.setCanSeeFriendlyInvisibles(seeInvisibleFriends);
+        if (teamColor != null)
+            team.setColor(teamColor);
+        if (displayName != null)
+            team.setDisplayName(ChatColor.translateAlternateColorCodes('&', displayName));
+        if (nameTagVisibility != null)
+            team.setNameTagVisibility(nameTagVisibility);
+        if (prefix != null)
+            team.setPrefix(ChatColor.translateAlternateColorCodes('&', prefix));
+        if (suffix != null)
+            team.setSuffix(ChatColor.translateAlternateColorCodes('&', suffix));
+    }
+
+    /**
+     * Removes the player from whatever team they were on
+     */
+    public static void resetTeam(Player player) {
+        // Get team the player is on
+        Team team = Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(player);
+        // If the player was on a team
+        if (team != null)
+            // Remove the player from that team
+            Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(player).removePlayer(player);
+    }
+
+    /**
+     * Removes all player tags (except tags for troubleshooting)
+     */
     public static void resetTags(Player player) {
         // Get copy of player's current tags
         Set<String> tagsToRemove = new HashSet<>(player.getScoreboardTags());
         // Remove all tags
         for (String tag : tagsToRemove)
-            if (!tag.equals("GameDev") && !tag.equals("testing"))      // TEMP... only used for data pack capabilities
+            if (!tag.equals("GameDev") && !tag.equals("testing"))      // game dev is TEMP... only used for data pack capabilities
                 player.removeScoreboardTag(tag);
     }
 
