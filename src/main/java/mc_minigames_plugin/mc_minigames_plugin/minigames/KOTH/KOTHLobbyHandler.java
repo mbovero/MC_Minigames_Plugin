fc28da7037ec61e9996a9aa6733015f239ff44cc
Kirt Robinson <kirtland.robinson@gmail.com>
2023-03-06 22:42:51 -0700

    Fixed kit changeing problem

--- src/main/java/mc_minigames_plugin/mc_minigames_plugin/minigames/KOTH/KOTHLobbyHandler.java
+++ src/main/java/mc_minigames_plugin/mc_minigames_plugin/minigames/KOTH/KOTHLobbyHandler.java
@@ -90,49 +90,48 @@ public class KOTHLobbyHandler extends PlayerArea implements Listener {
             //Check if the player has selected a valid kit entity and selects the specified class
                 //Damage kits
             if (kitName.equals("§4Striker"))  {       //Striker
-                player.addScoreboardTag("KOTH_kit_Striker");
+                changeKit(player, "KOTH_kit_Striker");
                 player.sendTitle("Striker", "Deal damage, and take it too");
             }
             if (kitName.equals("§4Orc")) {        //Orc
-                player.addScoreboardTag("KOTH_kit_Striker");
+                changeKit(player, "KOTH_kit_Striker");
                 player.sendTitle("Orc", "Mean and green with a devastating axe");
             }
             if (kitName.equals("§4Pyro")) {        //Pyro
-                player.addScoreboardTag("KOTH_kit_Pyro");
+                changeKit(player, "KOTH_kit_Pyro");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
             if (kitName.equals("§4Sayain")) {        //Saiyan
-                player.addScoreboardTag("KOTH_kit_Saiyan");
+                changeKit(player, "KOTH_kit_Saiyan");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
                 //Tank kits
             if (kitName.equals("§1Knight")) {        //Kight
-                player.addScoreboardTag("KOTH_kit_Tank");
+                changeKit(player, "KOTH_kit_Tank");
                 player.sendTitle("Knight", "Try pushing me off now");
             }
             if (kitName.equals("§1TMNT")) {        //TMNT
-                player.addScoreboardTag("KOTH_kit_TMNT");
+                changeKit(player, "KOTH_kit_TMNT");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
             if (kitName.equals("§1Trapper")) {        //Trapper
-                player.addScoreboardTag("KOTH_kit_Trapper ");
+                changeKit(player, "KOTH_kit_Trapper ");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
             if (kitName.equals("§4CockNBalls")) {        //Number 4
                 player.sendTitle("Sorry", "This class is not available yet");
-                player.sendTitle("Sorry", "This class is not available yet");
             }
                 //Ranged kits
             if (kitName.equals("§2Archer")) {        //Archer
-                player.addScoreboardTag("KOTH_kit_Archer");
+                changeKit(player, "KOTH_kit_Archer");
                 player.sendTitle("Archer", "Yeah, I shoot stuff");
             }
             if (kitName.equals("§2Sniper")) {        //Sniper
-                player.addScoreboardTag("KOTH_kit_Sniper");
+                changeKit(player, "KOTH_kit_Sniper");
                 player.sendTitle("Sniper", "Lol, get OPed");
             }
             if (kitName.equals("§aIce Spirit")) {        //Ice Spirit
-                player.addScoreboardTag("KOTH_kit_IceSpirit");
+                changeKit(player, "KOTH_kit_IceSpirit");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
             if (kitName.equals("§4CockNBalls")) {//Number 4
@@ -140,36 +139,36 @@ public class KOTHLobbyHandler extends PlayerArea implements Listener {
             }
                 //Magic kits
             if (kitName.equals("§dWarper")) {        //Warper
-                player.addScoreboardTag("KOTH_kit_Warper");
+               changeKit(player, "KOTH_kit_Warper");
                 player.sendTitle("Warper", "You're neither here nor there");
             }
             if (kitName.equals("§dClockmaster")) {        //Clock Master
-                player.addScoreboardTag("KOTH_kit_ClockMaster");
+                changeKit(player, "KOTH_kit_ClockMaster");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
             if (kitName.equals("§dwizard")) {        //Wizard
-                player.addScoreboardTag("KOTH_kit_Wizard");
+                changeKit(player, "KOTH_kit_Wizard");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
             if (kitName.equals("§dDruid")) {        //Druid
-                player.addScoreboardTag("KOTH_kit_Druid");
+                changeKit(player, "KOTH_kit_Druid");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
                 //Misc. kits
             if (kitName.equals("§3Fisherman")) {        //Fisherman
-                player.addScoreboardTag("KOTH_kit_Fisherman");
+                changeKit(player, "KOTH_kit_Fisherman");
                 player.sendTitle("Sorry", "Yank and smack, but don't tell your mom");
             }
             if (kitName.equals("§bOcean Man")) {        //Ocean man
-                player.addScoreboardTag("KOTH_kit_OceanMan");
+                changeKit(player, "KOTH_kit_OceanMan");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
             if (kitName.equals("§bBird Person")) {        //Bird Person
-                player.addScoreboardTag("KOTH_kit_BirdPerson");
+                changeKit(player, "KOTH_kit_BirdPerson");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
             if (kitName.equals("§bSpider")) {        //Spider Man
-                player.addScoreboardTag("KOTH_kit_SpiderMan");
+                changeKit(player, "KOTH_kit_SpiderMan");
                 player.sendTitle("Sorry", "This class is not available yet");
             }
         }
