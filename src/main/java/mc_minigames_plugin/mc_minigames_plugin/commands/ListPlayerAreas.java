ab51a0f9c0c53c6f6598ca235c53b52f4d2998f8
mbovero <milesbovero@icloud.com>
2023-05-09 12:41:44 -0600

    Small changes

--- src/main/java/mc_minigames_plugin/mc_minigames_plugin/commands/ListPlayerAreas.java
+++ src/main/java/mc_minigames_plugin/mc_minigames_plugin/commands/ListPlayerAreas.java
@@ -1,28 +1,22 @@
 package mc_minigames_plugin.mc_minigames_plugin.commands;
 
 import mc_minigames_plugin.mc_minigames_plugin.handlers.GeneralLobbyHandler;
-import mc_minigames_plugin.mc_minigames_plugin.minigames.GamePlayer;
 import mc_minigames_plugin.mc_minigames_plugin.minigames.KOTH.KOTHGameHandler;
 import mc_minigames_plugin.mc_minigames_plugin.minigames.KOTH.KOTHLobbyHandler;
 import mc_minigames_plugin.mc_minigames_plugin.minigames.PlayerArea;
-import mc_minigames_plugin.mc_minigames_plugin.util.TroubleshootUtil;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import static mc_minigames_plugin.mc_minigames_plugin.handlers.GeneralLobbyHandler.allPlayerAreas;
-import static mc_minigames_plugin.mc_minigames_plugin.handlers.GeneralLobbyHandler.findPlayerGlobal;
 
 /**
- * Command that give the player troubleshooting abilities:
- *  - Removes any existing restrictions on the player's actions
- *  - Right-click entities to get their name and location (printed to player and console)
+ * Provides functionality for the /listPlayerAreas command. This command iterates through every existing
+ * PlayerArea and lists the players within that area
  *
  * @author Miles Bovero
- * @version March 6, 2023
+ * @version May 9, 2023
  */
 public class ListPlayerAreas implements CommandExecutor {
 
@@ -32,7 +26,7 @@ public class ListPlayerAreas implements CommandExecutor {
         // Search allPlayerAreas (lobbies)
         for (PlayerArea area : allPlayerAreas) {
             // List collection data
-            Bukkit.getLogger().info(area.getAreaName() + area.getAreaPlayers().toString());
+            Bukkit.getLogger().info(area.getAreaName() + area.getAreaPlayers().keySet());
         }
 
         // Search KOTH games
@@ -40,7 +34,7 @@ public class ListPlayerAreas implements CommandExecutor {
             for (KOTHGameHandler KOTHGame : KOTHLobbyHandler.getActiveGames()) {
                 // List collection data
                 if (KOTHGame != null)
-                    Bukkit.getLogger().info(KOTHGame.getAreaName() + KOTHGame.getAreaPlayers().toString());
+                    Bukkit.getLogger().info(KOTHGame.getAreaName() + KOTHGame.getAreaPlayers().keySet());
             }
 
 
